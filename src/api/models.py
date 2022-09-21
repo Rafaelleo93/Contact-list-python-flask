11668cf6b507b05710e4d3ab25986fa61bdd8dfd
Rafaelleo93 <rafaelleonardo93@gmail.com>
2022-09-21 16:24:12 +0000

    1st part done

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,8 @@
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,4 +18,48 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+""" MODELOS """
+
+# Tabla intermedia de relación MANY-TO-MANY
+contact_group = db.Table('contact_group',
+                         db.Column("contact_id", db.Integer, db.ForeignKey(
+                             'contact.id'), primary_key=True),
+                         db.Column('group_id', db.Integer, db.ForeignKey(
+                             'group.id'), primary_key=True)
+                         )
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(60), nullable=False)  # Obligatorio
+    address = db.Column(db.String(120), nullable=True)
+    phone = db.Column(db.Integer, nullable=True)
+    update_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    groups = db.relationship('Group', secondary=contact_group, lazy='subquery',
+                             backref=db.backref('contacts', lazy=True))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "pokemon_name": self.full_name,
+            "email": self.email,
+            "address": self.address,
+            "phone": self.phone,
+            "groups": self.groups  # lista[1,2,3,4]
+        }
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)  # obligatorio
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "contacts": self.contact
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Contact, Group
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,53 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+""" RUTAS """
+
+
+@api.route('/contact/all', methods=['GET'])
+def get_contacts_all():
+    contacts = Contact.query.all()
+    serializer = list(map(lambda contact: contact.serialize(), contacts))
+
+    return jsonify({
+        "data": serializer
+    }), 200
+
+
+@api.route('/contact/<int:id>', methods=['GET'])
+def get_contact(id):
+    # coger el que coincide exactamente (query.get)
+    contact = Contact.query.get(id)
+    if contact is not None:
+        return jsonify({
+            "data": contact.serialize()
+        }), 200
+
+    return jsonify({"message": "El contacto no existe"}), 404
+
+
+@api.route('/contact/create', methods=['POST'])
+def create_contact():
+    data = request.get_json()
+    phone = data.get("phone", None)
+    name = data.get("name", None)
+
+    if phone is None:
+        return jsonify({"message": "El teléfono no puede estar vacío"}), 400
+    if name is None:
+        return jsonify({"message": "El nombre no puede estar vacío"}), 400
+
+    contact = Contact(
+        full_name=name,
+        phone=phone,
+        address=data.get('address'),
+        email=data.get('email'),
+        groups=data.get('groups', [])
+    )
+
+    db.session.add(contact)
+    db.session.commit()
+
+    return jsonify({"data": contact.serialize()}), 201
